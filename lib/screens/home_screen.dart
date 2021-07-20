28c0cdc67cf4d1a893c20fbab20e186e0186af71
simranmakhijani55@gmail.com <simranmakhijani55@gmail.com>
2021-07-21 01:42:50 +0530

    added tabs

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import '../pages/chat_page.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 4, vsync: this, initialIndex: 1);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Connect-O'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          PopupMenuButton(
+              onSelected: (value) {},
+              itemBuilder: (BuildContext context) {
+                return [
+                  PopupMenuItem(
+                    child: Text('New group'),
+                    value: 'New Group',
+                  ),
+                  PopupMenuItem(
+                    child: Text('New broadcast'),
+                    value: 'New broadcast',
+                  ),
+                  PopupMenuItem(
+                    child: Text('Whatsapp Web'),
+                    value: 'Whatsapp Web',
+                  ),
+                  PopupMenuItem(
+                    child: Text('Starred messages'),
+                    value: 'Starred messages',
+                  ),
+                  PopupMenuItem(
+                    child: Text('Settings'),
+                    value: 'Settings',
+                  ),
+                ];
+              }),
+        ],
+        bottom: TabBar(
+          controller: _controller,
+          tabs: [
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(
+              text: 'CHATS',
+            ),
+            Tab(
+              text: 'STATUS',
+            ),
+            Tab(
+              text: 'CALLS',
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _controller,
+        children: [
+          Text('camera'),
+          ChatPage(),
+          Text('status'),
+          Text('calls'),
+        ],
+      ),
+    );
+  }
+}
