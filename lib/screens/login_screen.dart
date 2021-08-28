c7809b0444e77285ad0c072eb8c65d316c53717d
Simran Makhijani <simranmakhijani55@gmail.com>
2021-08-29 02:39:20 +0530

    added dialog box

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -13,6 +13,7 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   String countryName = 'India';
   String countryCode = '+91';
+  TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -57,6 +58,28 @@ class _LoginScreenState extends State<LoginScreen> {
             countryCard(size),
             SizedBox(height: 5),
             number(),
+            Expanded(child: Container()),
+            InkWell(
+              onTap: () async {
+                if (_controller.text.length < 10) {
+                  await showMyDialog1();
+                } else {
+                  await showMyDialog();
+                }
+              },
+              child: Container(
+                color: Colors.tealAccent[400],
+                height: 40,
+                width: 70,
+                child: Center(
+                  child: Text(
+                    'NEXT',
+                    style: TextStyle(fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
           ],
         ),
       ),
@@ -107,6 +130,7 @@ class _LoginScreenState extends State<LoginScreen> {
     return Container(
       width: size.width / 1.5,
       height: 38,
+      padding: EdgeInsets.symmetric(vertical: 5),
       child: Row(
         children: [
           Container(
@@ -121,8 +145,9 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
             child: Row(
               children: [
+                SizedBox(width: 10),
                 Text('+', style: TextStyle(fontSize: 18)),
-                SizedBox(width: 20),
+                SizedBox(width: 15),
                 Text(countryCode.substring(1), style: TextStyle(fontSize: 15)),
               ],
             ),
@@ -139,6 +164,7 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
             ),
             child: TextFormField(
+              controller: _controller,
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
                 border: InputBorder.none,
@@ -158,5 +184,79 @@ class _LoginScreenState extends State<LoginScreen> {
       countryCode = country.code;
     });
     Navigator.of(context).pop();
+  }
+
+  Future<void> showMyDialog() {
+    return showDialog(
+        context: context,
+        builder: (ctx) {
+          return AlertDialog(
+            content: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'We will be verifying your phone number',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    countryCode + ' ' + _controller.text,
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Is this Ok, or would you like to edit the number?',
+                    style: TextStyle(fontSize: 13.5),
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Edit'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Ok'),
+              ),
+            ],
+          );
+        });
+  }
+
+  Future<void> showMyDialog1() {
+    return showDialog(
+        context: context,
+        builder: (ctx) {
+          return AlertDialog(
+            content: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'There is no number you entered',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Ok'),
+              ),
+            ],
+          );
+        });
   }
 }
